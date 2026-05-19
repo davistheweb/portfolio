a1e9323a145f90fb3d450a3e231f5e39c8ddf689
davistheweb <josiahdave001@gmail.com>
2026-05-19 21:27:54 +0100

    :

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect } from "react";
-export default function page() {
+export default function ContactPage() {
   useEffect(() => {
     window.addEventListener("load", () =>
       window.open("https://wa.me/+2349018797128"),
